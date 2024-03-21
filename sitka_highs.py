e65f9ceec4a394403ac6df85d8d3eb6d7c883e2a
pavigodj <saturnturn@yahoo.com>
2024-03-21 11:53:20 -0700

    commit 3

--- sitka_highs.py
+++ sitka_highs.py
@@ -1,19 +1,28 @@
 import csv 
 
-filename = "/Users/bavi/Documents/DataVizWheather/data/sitka_weather_2018_simple.csv"
+import matplotlib.pyplot as plt
+
+filename = "/Users/bavi/Documents/DataVizWheather/Whether-data-visulatization/data/sitka_weather_2018_simple.csv"
 
 
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
- #   print(header_row)
     
   #  for index,column_header in enumerate(header_row):
   #      print(index, column_header)
 
-    highTemp = [int(temp[5]) for temp in reader]
-#for tem in header_row:
-#   t = int(tem[5])
-#   highTemp.append(t)
+    highs = [int(temp[5]) for temp in reader]
+plt.style.use('seaborn')
+fig, ax = plt.subplots()
+ax.plot(highs, c = 'red')
 
-print(highTemp)
+#Format Plot
+plt.title("Daily high temperatures, July 2018", fontsize =24)
+plt.xlabel('', fontsize =16)
+plt.ylabel("Tempature (F)", fontsize =16)
+plt.tick_params(axis = 'both', which ='major', labelsize =16)
+
+plt.show()
+
+# print(highs)
